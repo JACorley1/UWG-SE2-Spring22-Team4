037b85419b95982583606d5095938a43977e6f7e
Tyler Jones <76069841+wjones17@users.noreply.github.com>
2022-02-09 16:03:14 -0500

    created a login page as well as structured the files for the project

---
+++ code/view/LoginController.java
@@ -0,0 +1,23 @@
+package code.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+    @FXML
+    private TextField userNameTextfield;
+
+    @FXML
+    private TextField passwordTextfield;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    void handleLogin(ActionEvent event) {
+
+    }
+
+}
