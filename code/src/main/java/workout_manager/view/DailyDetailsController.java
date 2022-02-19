e13ff510c3cb3df38aeab578c018ee935bbcd0f4
Tyler Jones <76069841+wjones17@users.noreply.github.com>
2022-02-19 15:18:31 -0500

    created a daily detail view for daily workouts and controller

---
+++ code/src/main/java/workout_manager/view/DailyDetailsController.java
@@ -0,0 +1,38 @@
+package workout_manager.view;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.stage.Stage;
+import workout_manager.Main;
+
+public class DailyDetailsController {
+
+    @FXML
+    private TextArea workoutTextArea;
+
+    @FXML
+    private Label dayLabel;
+
+    @FXML
+    private Button backButton;
+
+    @FXML
+    void handleBackButton(ActionEvent event) throws IOException {
+        Stage stage = (Stage) this.backButton.getScene().getWindow();
+        Parent parent = FXMLLoader.load(Main.class.getResource(Main.PREFERENCE_PAGE));
+        Scene scene = new Scene(parent);
+        stage.setTitle(Main.WINDOW_TITLE);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+}
