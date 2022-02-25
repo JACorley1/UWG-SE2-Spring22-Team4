af42c8b1229f143182121ef0445ba518307a7496
balton818 <balton1@my.westga.edu>
2022-02-25 18:35:03 -0500

    Completed testing for LocalLogin

--- code/src/test/java/workout_manager/test/model/TestLocalLoginAuthenticator.java
+++ code/src/test/java/workout_manager/test/model/TestLocalLoginAuthenticator.java
@@ -1,6 +1,7 @@
 package workout_manager.test.model;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
@@ -9,12 +10,54 @@ import workout_manager.model.LocalLoginAuthenticator;
 public class TestLocalLoginAuthenticator {
     
     @Test
+    void testShouldNotAllowNullUsername() {
+        LocalLoginAuthenticator verify = new LocalLoginAuthenticator();
+        assertThrows(IllegalArgumentException.class, () -> verify.authenticateLoginCredentials(null, "password"));
+    }
+
+    @Test
+    void testShouldNotAllowEmptyUsername() {
+        LocalLoginAuthenticator verify = new LocalLoginAuthenticator();
+        assertThrows(IllegalArgumentException.class, () -> verify.authenticateLoginCredentials("", "password"));
+    }
+
+    @Test
+    void testShouldNotAllowNullPassword() {
+        LocalLoginAuthenticator verify = new LocalLoginAuthenticator();
+        assertThrows(IllegalArgumentException.class, () -> verify.authenticateLoginCredentials("username", null));
+    }
+
+    @Test
+    void testShouldNotAllowEmptyPassword() {
+        LocalLoginAuthenticator verify = new LocalLoginAuthenticator();
+        assertThrows(IllegalArgumentException.class, () -> verify.authenticateLoginCredentials("username", ""));
+    }
+
+    @Test
     void testAuthenticatesLoginInfo() {
         LocalLoginAuthenticator verify = new LocalLoginAuthenticator();
         String username = "username";
         String password = "password";
 
         assertEquals(true, verify.authenticateLoginCredentials(username, password));
+    }
+
+    @Test
+    void testDoesNotAuthenticateInvalidCredentials() {
+        LocalLoginAuthenticator verify = new LocalLoginAuthenticator();
+        assertEquals(false, verify.authenticateLoginCredentials("Toby", "Keith"));
+    }
+
+    @Test
+    void testDoesNotAuthenticateValidUsernameWithInvalidPassword() {
+        LocalLoginAuthenticator verify = new LocalLoginAuthenticator();
+        assertEquals(false, verify.authenticateLoginCredentials("username", "BigDaddy56"));
+    }
+
+    @Test
+    void testDoesNotAuthenticateInvalidUsernameWithValidPassword() {
+        LocalLoginAuthenticator verify = new LocalLoginAuthenticator();
+        assertEquals(false, verify.authenticateLoginCredentials("Bobby", "password"));
     }
     
 }
