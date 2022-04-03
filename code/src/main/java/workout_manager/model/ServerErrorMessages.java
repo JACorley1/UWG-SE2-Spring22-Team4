ef66fe4e0dd151641a3591280d56ce768cc756ae
balton818 <balton1@my.westga.edu>
2022-04-03 15:19:55 -0400

    Added documentation
    - Added class documentation for ServerError... and
    constrained the Accordion on DailyDetails

--- code/src/main/java/workout_manager/model/ServerErrorMessages.java
+++ code/src/main/java/workout_manager/model/ServerErrorMessages.java
@@ -1,5 +1,11 @@
 package workout_manager.model;
 
+/**
+ * a class of static server error messages
+ * 
+ * @author Ben Alon
+ * @version Spring 2022 
+ */
 public class ServerErrorMessages {
 
     public static final String LOGIN_FAILED = "100";
